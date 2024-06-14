b362e27dc7631f2017a26e2abacd57e90956ec42
kdh1004 <50905795+kdh1004@users.noreply.github.com>
2024-06-15 03:49:24 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    
+    int n;
+    char s[101];
+    
+    scanf("%d",&n);
+    
+    while(n--){
+        scanf("%s", s);
+        
+        printf("%c%c\n", s[0], s[strlen(s)-1]);
+    }
+}
