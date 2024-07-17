17998a287b0a3186d230ec9c67f812e97e5584e0
kdh1004 <50905795+kdh1004@users.noreply.github.com>
2024-07-17 21:21:34 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = []
+    odd,even = 0,0
+    for num in num_list:
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return [even,odd]
+    
