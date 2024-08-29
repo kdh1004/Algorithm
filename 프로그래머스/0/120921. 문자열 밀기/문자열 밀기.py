8d5c6e96127957be179a7b5b23274e067d509fae
kdh1004 <50905795+kdh1004@users.noreply.github.com>
2024-08-29 18:52:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    if A == B:
+        return 0
+    for i in range(1,len(A)):
+        A = A[-1] + A[0:-1]
+        print(A)
+        if A == B:
+            return i
+    return -1
+    return answer
