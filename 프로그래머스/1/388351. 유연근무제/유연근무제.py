11240550c99ddd9b2295441a1c6576a69e67a88c
Kim DongHae <50905795+kdh1004@users.noreply.github.com>
2026-01-05 23:27:58 +0900

    [level 1] Title: 유연근무제, Time: 1.58 ms, Memory: 9.51 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -0,0 +1,26 @@
+def ctime(n):
+    h = n // 100
+    m = n % 100
+    return h * 60 + m
+
+def solution(schedules, timelogs, startday):
+    answer = 0
+    
+    for i in range(len(schedules)):
+        s = startday
+        schedule = ctime(schedules[i])
+        for time in timelogs[i]:
+            if s in [6,7]:
+                s += 1
+                if s == 8:
+                    s = 1
+                continue
+            t = ctime(time)
+            if schedule + 10 < t:
+                break
+            else:
+                s += 1
+        else:
+            answer += 1
+            
+    return answer
